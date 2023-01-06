d6d746a3bdaa43047a2990ef5e69ec3847abab59
Marcos Coca <marcoscoca0905@gmail.com>
2023-01-05 23:37:10 -0400

    add types and move cypress tests

--- cypress/components/indexHome.cy.tsx
+++ cypress/components/indexHome.cy.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import React from "react";
-import Home from "../../pages/index";
+import Home from "../../src/pages/index";
 
 describe("<Home />", () => {
   it("renders", () => {
